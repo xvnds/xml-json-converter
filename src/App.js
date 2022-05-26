949210ce95ebcc5e5f97a6c54867677359a82d3e
Xandy Reyes <xands@outlook.com>
2022-05-27 01:14:21 +0800

    Use xml2js lib

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
-import { Button, Row, Col, Container } from "react-bootstrap";
+import { Alert, Button, Col, Container, Form, Row} from "react-bootstrap";
 import CodeEditor from '@uiw/react-textarea-code-editor';
+import { parseString } from "xml2js";
 
+const parser = new DOMParser();
 function App() {
   const [xml, setXml] = useState(`
     {
@@ -9,28 +11,79 @@ function App() {
       "arr": [0, 1, 2, 3, 4]
     }
   `);
+  const [textareaVal, setTextareaVal] = useState("");
+  const [targetTagname, setTargetTagname] = useState("");
+  const [error, setError] = useState();
+
+  const onClickConvert = () => {
+    try {
+      setError();
+      if (textareaVal === "") {
+        setError("No XML value.");
+        return;
+      }
+      const xmlDoc = parser.parseFromString(textareaVal, "text/xml");
+      const responseData = xmlDoc.getElementsByTagName(targetTagname);
+      const data = targetTagname !== "" ? responseData[0].textContent : textareaVal;
+      parseString(data, (_err, result) => {
+        setXml(JSON.stringify(result, null, 4));
+      });
+    } catch (err) {
+      console.log(err);
+      setError("Invalid tagname.");
+    }
+  }
+
+  const onClickClear = () => {
+    setTextareaVal("");
+    setXml("");
+    setTargetTagname("");
+    setError();
+  }
+
   return (
     <div className="App">
       <div className="w-100">
         <Container fluid className="p-5">
           <Row>
+            <Col><h1>XML to JSON Converter</h1></Col>
+          </Row>
+          <Row className="mt-4">
+            <Col><Form.Control placeholder="Target tag name" onChange={(e) => setTargetTagname(e.target.value)} value={targetTagname}/></Col>
+          </Row>
+          {error && (
+            <Row className="mt-2">
+              <Col><Alert variant="danger">{error}</Alert></Col>
+            </Row>
+          )}
+          <Row className="mt-2">
             <Col>
-              <textarea className="w-100" rows="30"/>
-              <Button className="w-100">Hello</Button>
+              <textarea className="w-100" rows="30" value={textareaVal} onChange={(e) => setTextareaVal(e.target.value)}/>
+              <Row className="mt-3">
+                <Col><Button className="w-100" onClick={onClickClear} variant="secondary">Clear</Button></Col>
+                <Col>
+                  <Button
+                    className="w-100"
+                    onClick={onClickConvert}
+                  >Convert to JSON</Button>
+                </Col>
+              </Row>
             </Col>
             <Col>
-              <textarea className="w-100" rows="20" value={xml} />
               <CodeEditor
                 value={xml}
-                language="json"
-                placeholder="Please enter JS code."
+                language="webmanifest"
+                placeholder="JSON result will be shown here"
                 onChange={(evn) => setXml(evn.target.value)}
                 padding={5}
                 style={{
                   fontSize: 14,
-                  backgroundColor: "#f0f0f0",
+                  backgroundColor: "#202020",
                   fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+                  maxHeight: 790,
+                  overflowY: "scroll"
                 }}
+                minHeight={790}
               />
             </Col>
           </Row>
